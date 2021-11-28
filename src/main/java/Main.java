6ad649bb1487bedd6564a7d271d3986b48940487
Khelmyanov_Alexander <alkhelmyanov@gmail.com>
2021-11-28 17:27:29 +0300

    Create Home work #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,36 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет мир");
+       /* int a = 1;
+        long l = 1000000000L;
+        float f = 12.3f;
+        double d = 12.43;
+        String s = "Привет черти";
+        boolean b = true;
+        System.out.println(a);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(s);
+        System.out.println(b);*/
+
+        System.out.println(vichisleniePrimera(2, 5, 30, 2));
+        System.out.println(sravnenieChisel(16, 5));
     }
+
+    private static int vichisleniePrimera(int a, int b, int c, int d) {
+        return a * (b + (c / d));
+    }
+
+    private static boolean sravnenieChisel(int a, int b) {
+        int summa = a + b;
+        if (summa >= 10 && summa <= 20) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
 }
+
+

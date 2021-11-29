1ab42d806a5f440c676761a12ed60bd1f7f2733b
Khelmyanov_Alexander <alkhelmyanov@gmail.com>
2021-11-29 23:39:27 +0300

    try switch

--- src/main/java/HomeWork2/HomeWork2.java
+++ src/main/java/HomeWork2/HomeWork2.java
@@ -2,14 +2,29 @@ package HomeWork2;
 
 public class HomeWork2 {
     public static void main(String[] args) {
-        int c = 1;
-        System.out.println(c++ + c); // 1 + 2 // сначала увеличили c на 1 потом прибавили 1
-        System.out.println("теперь с равна " + c); // с == 2
-        System.out.println(++c + c); // 3 + 3 // сначала увеличили с на 1
-        System.out.println("теперь с равна " +c); // с == 3
-        System.out.println(c + c++);
-        System.out.println("теперь с равна " +c); // с == 4
-        c += ++c + c;
-        System.out.println("теперь с равна " +c); // с == 14
+
+    /*    int nujnoeZnachenie = sumAndVars(10,20);
+        System.out.println("Результат выполнения метода " + nujnoeZnachenie);
     }
+
+    private static int sumAndVars(int a, int b) {
+    int result = a + b;
+    return result;*/
+        converSeasonSwitch(2);
+
+    }
+
+    public static void converSeasonSwitch(int seasonNumber) {
+        String season = switch (seasonNumber) { // сразу переменной season присваивается возврат switch
+            case 1, 5, 6 -> "Winter"; // можно проверять несколько аргументов seasonNumber
+            case 2 -> "Spring";
+            case 3 -> "Summer";
+            case 4 -> "Fall";
+            default -> "Unknown season";
+        };
+
+        System.out.println(season);
+
+    }
+
 }
